e858659cd20d369b37b4cad37616195df7e3fa47
awasdz0041 <71378266+awasdz0041@users.noreply.github.com>
2020-09-17 09:12:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ const Telegraf  = require("telegraf");
 const bot       = new Telegraf(BOT_API);
 
 const buton = { reply_markup: { inline_keyboard: [[ { text: 'Kanal', url: 't.me/botsohbet' } ]] }}
-
-bot.hears(/bota verilen cevap/ig, async (ctx, next) => {
-        await ctx.replyWithHTML('Sorular ve Cevaplar', buton)
-    return next();
     
-
 bot.start((ctx) => { return ctx.reply("Aşağıdaki kodları kullanarak botu kullanabilirsiniz.\n/yardim - Bot kullanım kılavuzunu açar\n/yenisoru - Yeni soru sormak için ilk önce komutu yazmalısın.")
     
 });
